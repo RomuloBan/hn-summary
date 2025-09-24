88f2ea95d941cd7a10370c5be521795e6c5dd9fb
romuloban <romulo88@hotmail.com>
2025-09-24 23:00:18 +0200

    add initial commit

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import { Hono } from "hono";
+import { jsxRenderer } from "hono/jsx-renderer";
+
+const app = new Hono();
+
+app.use(
+  "*",
+  jsxRenderer(({ children }) => {
+    return (
+      <html>
+        <head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+          />
+        </head>
+        <body>
+          <header>Wow Renderer!</header>
+          <div>{children}</div>
+        </body>
+      </html>
+    );
+  }),
+);
+
+app.get("/", (c) => {
+  return c.render(<h1>Hello World!</h1>);
+});
+
+export default app;
